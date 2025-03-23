9d8d2d48b4a1c975148ea7a234b2a319c0a64eed
Bautista <bautichehin@gmail.com>
2025-03-23 16:47:05 -0300

    Generacion de Componente product y mapeo en List

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,29 @@
+// src/components/ProductItem.tsx
+import React from "react";
+import { ProductStatus } from "../models/ProductStatus";
+import Product from "../models/Product";
+
+interface ProductItemProps {
+  product: Product;
+}
+
+const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: product.status === ProductStatus.Active ? "green" : "red",
+          width: "10px",
+          height: "10px",
+          borderRadius: "50%",
+        }}
+      />
+      <h3>{product.name}</h3>
+      <p>{product.category}</p>
+      <p>{product.getFormattedPrice()}</p>
+      <p>Stock: {product.stock}</p>
+    </div>
+  );
+};
+
+export default ProductItem;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import { Product } from '../models/Product';
+import React from "react";
+import Product from "../models/Product";
+import ProductItem from "./ProductItem";
 
 interface ProductListProps {
-  productList: Product[];
+  products: Product[];
 }
 
-export const ProductList: React.FC<ProductListProps> = ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  productList,
-}) => {
-  return <div />;
+const ProductList: React.FC<ProductListProps> = ({ products }) => {
+  return (
+    <div>
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
+
+export default ProductList;
